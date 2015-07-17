ee06c30f223ac27fe898c512c50904b897a19d1e
nabe-abk <nabe-g@abk.nu>
2015-07-17 20:18:43 +0900

    Add: 'nature-dark' theme.

---
+++ theme/satsuki2/nature-dark/README.txt
@@ -0,0 +1,15 @@
+Title: nature-dark
+Template: satsuki2
+Author: nabe@abk
+Access: https://twitter.com/nabe_abk
+License: GPL
+Charset: UTF-8
+Smartphone: yes
+System-mode: yes
+
+・natureベースですが配色はオリジナルです。
+
+・ヘッダに背景画像をはめ込んで使用してください。
+
+ChangeLog
+2015/07/17 初版
---
+++ theme/satsuki2/nature-dark/nature-dark.js
@@ -0,0 +1,34 @@
+//############################################################################
+// 記事ヘッダ加工用
+//							(C)2015 nabe@abk
+//############################################################################
+//[TAB=8]  require jQuery
+'use strict';
+
+$(function() {
+	$('#main-first article.article').each(function(idx,dom){
+		var obj = $(dom);
+		var h2   = obj.find('h2');
+		var date = h2.find('a.date');
+		var head = obj.find('div.body-header');
+		if (h2.length && head.length) {
+			h2.append(date);
+			h2.append(head);
+		}
+		var title    = obj.find('h2');
+		var bookmark = h2.find('a.http-bookmark');
+		if (title.length && bookmark.length) {
+			title.append(bookmark);
+		}
+	});
+});
+
+
+
+
+
+
+
+
+
+
