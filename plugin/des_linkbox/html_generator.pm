3ff1c85d6cbddf0fc0c5ba6154608a9f16085ecc
nabe-abk <nabe-g@abk.nu>
2015-07-22 13:19:08 +0900

    Update: sidebar plugins.

--- plugin/des_linkbox/html_generator.pm
+++ plugin/des_linkbox/html_generator.pm
@@ -4,12 +4,13 @@
 sub {
 	my $self = shift;
 	my $name = shift;
+	my $id   = $self->plugin_name_id($name);
 	my $set  = $self->load_plgset($name);
 
 	my $title = $set->{title} || 'link list';
 	my $html = <<HTML;
 <!--Links=========================================-->
-<div class="hatena-module side-link-list" data-module-name="$name">
+<div class="hatena-module side-link-list" data-module-name="$name" id="$id">
 <div class="hatena-moduletitle">$title</div>
 <div class="hatena-modulebody">
 <ul class="hatena-section">
