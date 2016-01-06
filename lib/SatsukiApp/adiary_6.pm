5b0e52adf5b5a1befa0b345284a64002a87da0cc
nabe-abk <nabe-g@abk.nu>
2016-01-06 23:26:51 +0900

    Fix: wrong message main view by date mode.

--- lib/SatsukiApp/adiary_6.pm
+++ lib/SatsukiApp/adiary_6.pm
@@ -21,7 +21,8 @@ my @update_versions = (
 	{ ver => 2.96, func => 'sys_update_296', plugin=>1, theme=>1   },
 	{ ver => 2.97, func => 'sys_update_297', plugin=>1, theme=>1, rebuild=>1 },
 	{ ver => 2.98, plugin=>1, theme=>1 },
-	{ ver => 3.00, plugin=>1 }
+	{ ver => 3.00, plugin=>1 },
+	{ ver => 3.01, plugin=>1 }
 );
 #------------------------------------------------------------------------------
 # ●システムアップデート
