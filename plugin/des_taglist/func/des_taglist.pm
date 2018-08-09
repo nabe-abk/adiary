3f5feaf059d7f6fcb8536514729027fed38d751a
nabe-abk <339449+nabe-abk@users.noreply.github.com>
2018-08-09 14:18:57 +0900

    Update: des_taglis selectable display node

--- plugin/des_taglist/func/des_taglist.pm
+++ plugin/des_taglist/func/des_taglist.pm
@@ -4,13 +4,27 @@
 sub {
 	my $self = shift;
 	my $name = shift;
-	my $tree = shift;
+	my $root = shift;
 	my $ROBJ = $self->{ROBJ};
 	my $DB   = $self->{DB};
 	my $blogid = $self->{blogid};
 
 	if ($self->{event_name} ne 'TAG_STATE_CHANGE') {
-		$tree = $self->load_tag_tree( $blogid );
+		$root = $self->load_tag_tree( $blogid );
+	}
+
+	my $node = $self->load_plgset($name, 'node');
+	my $id   = $self->load_plgset($name, 'id');
+	my $tree = {};
+	if ($node) {
+		my $all = $root->{_all};
+		foreach(@$all) {
+			if ($_->{name} ne $node) { next; }
+			$tree = $_;
+			last;
+		}
+	} else {
+		$tree = $root;
 	}
 
 	# タグのない記事を探す
