f2d13849981858bf89d9d8cecf1f2bf2b6aa9c85
nabe-abk <339449+nabe-abk@users.noreply.github.com>
2018-08-11 22:52:00 +0900

    Update: for Ver3.16

--- lib/SatsukiApp/adiary.pm
+++ lib/SatsukiApp/adiary.pm
@@ -7,9 +7,9 @@ package SatsukiApp::adiary;
 use Satsuki::AutoLoader;
 use Fcntl ();
 #-------------------------------------------------------------------------------
-our $VERSION = 3.15;
-our $OUTVERSION = '3.15';
-our $SUBVERSION = '-C94SE';
+our $VERSION = 3.16;
+our $OUTVERSION = '3.16';
+our $SUBVERSION = '';
 our $DATA_VERSION = 3.15;
 ###############################################################################
 # ■システム内部イベント
