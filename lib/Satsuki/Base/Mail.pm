cce25082866a3e5cda3e3c15dd5d6c6356ca5c1a
nabe-abk <339449+nabe-abk@users.noreply.github.com>
2019-08-09 15:55:55 +0900

    Update: mail.pm, remove parse_mail() routine

--- lib/Satsuki/Base/Mail.pm
+++ lib/Satsuki/Base/Mail.pm
@@ -9,8 +9,8 @@ our $VERSION = '1.20';
 use Socket;
 #------------------------------------------------------------------------------
 my $TIMEOUT = 5;
-my $CODE    = 'UTF-8';
-my $DEBUG   = 0;
+my $CODE;
+my $DEBUG = 0;
 my %Auth;
 ###############################################################################
 # ■基本処理
@@ -158,6 +158,7 @@ sub send_mail {
 				die("AUTH $type failed: \"$h->{auth_name}\" / \"$h->{auth_pass}\" : $@")
 			}
 		}
+		$from ||= $to;
 		$self->send_data_check($sock, "MAIL FROM:$from", 250);
 		$self->send_data_check($sock, "RCPT TO:$to", 250);
 		if ($cc) {
@@ -348,163 +349,6 @@ sub mail_date_local {
 }
 
 ###############################################################################
-# ■メールの解析
-###############################################################################
-#------------------------------------------------------------------------------
-# ●メールの解析
-#------------------------------------------------------------------------------
-sub parse_mail {
-	my $self = shift;
-	my $ary  = shift;
-	
-	# 文字コード変換
-	my $ROBJ  = $self->{ROBJ};
-	my $code  = shift || $ROBJ->{System_coding};
-	my $jcode = $ROBJ->load_codepm();
-	
-	# ヘッダ解析
-	my $mail = $self->parse_mail_header($ary, $code);
-	# 追加解析（From, Toを判別（最初の１人のみ））
-	if ($mail->{from} =~ /<([\w\.\-]+\@[\w\.\-]+)>/ || $mail->{from} =~ /([\w\.\-]+\@[\w\.\-]+)/) {
-		$mail->{from_address} = $1;
-	}
-	if ($mail->{to} =~ /<([\w\.\-]+\@[\w\.\-]+)>/ || $mail->{to} =~ /([\w\.\-]+\@[\w\.\-]+)/) {
-		$mail->{to_address} = $1;
-	}
-	if ($mail->{reply_to} =~ /<([\w\.\-]+\@[\w\.\-]+)>/ || $mail->{reply_to} =~ /([\w\.\-]+\@[\w\.\-]+)/) {
-		$mail->{reply_to_address} = $1;
-	}
-
-	#----------------------------------------
-	# 本文の解析
-	#----------------------------------------
-	my $boundary;
-	{
-		my $from_code='JIS';
-		my $type = $mail->{content_type};
-		if ($type =~ /charset="(.*?)"/i) { $from_code=$1; }
-		if ($type !~ m#^multipart/\w+;\s*boundary=(?:"(.*?)"|([^\s]*))#i) {
-			if ($self->{debug}) { print "[Mail.pm] mail is simple\n"; }
-			my $text = join('', @$ary);
-			$text = $self->decode_quoted_printable($mail->{content_transfer_encoding}, $text);
-			$jcode->from_to(\$text, $from_code, $code);
-			$text = $self->decode_rfc3676( $type, $text );
-			$mail->{text} = $text;
-			return $mail;
-		}
-		$boundary = "--$1$2";
-	}
-
-	#----------------------------------------
-	# マルチパートの解析
-	#----------------------------------------
-	my $b1 = $boundary;
-	my $b2 = "$boundary--";
-	if ($self->{debug}) { print "[Mail.pm] mail is multipart\n"; }
-
-	my @attaches;
-	$mail->{attaches} = \@attaches;
-	my $count=0;
-	while(@$ary) {
-		my $x = shift(@$ary);
-		$x =~ s/[\r\n]//g;
-		if ($x ne $boundary && $x ne $b2) { next; }
-		while(@$ary) {
-			my $h = $self->parse_mail_header($ary);
-			my $type   = $h->{content_type};
-			my $encode = $h->{content_transfer_encoding};
-			# 添付ファイル
-			$encode =~ tr/A-Z/a-z/;
-			if ($encode eq 'base64') {
-				$h->{filename} = 'file' . (++$count);
-				# 添付ファイル名は Content-Disposition を優先する
-				my $x = $self->parse_header_line( $h->{content_disposition}, $code );
-				if (exists $x->{filename}) {
-					$h->{filename} = $x->{filename};
-				} else {
-					# Content-type からファイル名取得
-					my $x = $self->parse_header_line( $type, $code );
-					if (exists $x->{name}) {
-						$h->{filename} = $x->{name};
-					}
-				}
-				$h->{data} = $self->read_until_boundry($ary, $boundary, 1);
-				if ($self->{debug}) {
-					print "[Mail.pm] Attachement file: $h->{filename} ",length($h->{data})," byte\n\n";
-				}
-				push(@attaches, $h);
-
-			# textメール文、htmlメール文
-			} elsif (($encode eq '' || $encode eq '7bit' || $encode eq '8bit' || $encode =~ /quoted-printable/)
-			      && ($type =~ m|^(text)/plain;| || $type =~ m|^text/(html);|)) {
-				my $ctype = $1;
-				my $v = $self->read_until_boundry($ary, $boundary);
-				$v = $self->decode_quoted_printable($encode, $v);
-				# 文字コード変換
-				my $from_code = 'JIS';
-				if ($type =~ /charset="(.*?)"/i || $type =~ /charset=([^\s;]*)/i) { $from_code=$1; }
-				$jcode->from_to(\$v, $from_code, $code);
-				$v = $self->decode_rfc3676( $type, $v );
-				$mail->{$ctype} = $v;
-				if ($self->{debug}) {
-					print "\n\n$ctype=$v\n";
-				}
-			}
-		}
-	}
-	return $mail;
-}
-
-#-----------------------------------------------------------
-# ○multipart を境界まで読む
-#-----------------------------------------------------------
-sub read_until_boundry {
-	my ($self, $ary, $boundary, $base64) = @_;
-	my $b2 = "$boundary--";
-	my $data;
-	while(@$ary) {
-		my $x = shift(@$ary);
-		$x =~ s/[\r\n]//g;
-		if ($x eq $boundary || $x eq $b2) { last; }
-		if ($base64) { $data .= $self->base64decode($x); next; }
-		$data .= "$x\n";
-	}
-	return $data;
-}
-
-#------------------------------------------------------------------------------
-# ●メールヘッダの解析
-#------------------------------------------------------------------------------
-sub parse_mail_header {
-	my ($self, $ary, $code) = @_;
-	my %h;
-	my ($n, $v);
-	while(@$ary) {
-		my $x = shift(@$ary);
-		$x =~ s/[\r\n]//g;
-		if ($x =~ /^([ \t]+.*)/) { $v.=$1; next; } # RFC 2822 FWS / RFC 2234 WSP
-		if (defined $n) {
-			$n =~ tr/A-Z\-/a-z_/;
-			if (defined $code) {
-				$v = $self->mime_decode_line($v, $code);
-			}
-			$h{$n} = $v;
-			if ($self->{debug}) {
-				print "[Mail.pm] Header: $n=$v\n";
-			}
-			undef $n;
-		}
-		# 新たなヘッダ
-		if ($x =~ /^([\w\-]+):\s*(.*)/) {
-			$n = $1;
-			$v = $2;
-		}
-		if ($x eq '') { last; }
-	}
-	return \%h;
-}
-
-###############################################################################
 # ■MIME base64エンコード/デコード
 ###############################################################################
 my $base64table = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
@@ -556,113 +400,6 @@ sub base64encode {
 #------------------------------------------------------------------------------
 # ●デコード
 #------------------------------------------------------------------------------
-sub mime_decode_line {
-	my $self = shift;
-	my $line = shift;
-	my $code = shift;
-	my $ROBJ = $self->{ROBJ};
-
-	if ($line !~ /=\?.*\?=/) { return $line; }
-	$line =~ s/\x00//g;
-	my @buf;
-	my $mime_code;
-	# MIMEデコードをしてエスケープ表記"\0 num \0"に置き換え
-	$line =~ s/=\?([\w\-]*)\?[Bb]\?([A-Za-z0-9\+\/=]*)\?=/
-		$mime_code = $1;
-		push(@buf, $self->base64decode($2));
-		"\x00$#buf\x00";
-	/eg;
-	# RFC 2047
-	$line =~ s/\x00[\t ]+\x00/\x00\x00/g;
-	# buffer復元
-	$line =~ s/\x00(\d+)\x00/$buf[$1]/g;
-	# 文字コード変換
-	if ($mime_code && $code) {
-		my $jcode = $ROBJ->load_codepm();
-		$jcode->from_to(\$line, $mime_code, $code);
-	}
-	return $line;
-}
-sub parse_header_line {		# RFC2231準拠
-	my $self = shift;
-	my $line = shift;
-	my $code = shift;
-	my $ROBJ = $self->{ROBJ};
-	my $jcode = $ROBJ->load_codepm();
-
-	# stringの保存
-	my @str;
-	$line =~ s/"(.*?)"/push(@str, $1), "\x00$#str\x00"/eg;
-
-	my %h;
-	foreach(split(/\s*;\s*/, $line)) {
-		# str復元
-		$_ =~ s/\x00(\d+)\x00/$str[$1]/g;
-		if ($_ =~ /^\s*(.*?)=(.*?)\s*$/) {
-			my $key = $1;
-			my $val = $2;
-			$key =~ tr/-/_/;
-			if ($key =~ /^(.*?\*)\d+\*?$/) {
-				$key = $1;
-				$h{$key} .= $val;
-			} else {
-				$h{$key} = $val;
-			}
-		} elsif (!exists $h{_}) {
-			$h{_} = $_;
-		}
-	}
-	foreach(keys(%h)) {
-		# RFC2231 '*'表記の展開とMIME処理
-		#（例）filename*=iso-2022-jp''%1B%24B%3CL%3F%3F%1B%28B.jpg
-		my $val = $h{$_};
-		if ($_ =~ /^(.*?)\*$/) {
-			my $key = $1;
-			delete $h{$_};
-			if ($val =~ /^(.*?)'.*?'(.*)$/) {
-				my $val_code = $1;
-				$val = $2;
-				$val =~ s/%([0-9a-fA-F][0-9a-fA-F])/chr(hex($1))/eg;
-				$jcode->from_to(\$val, $val_code, $code);
-			}
-			$h{$key} = $val;
-		} else {
-			$h{$_} = $self->mime_decode_line($val, $code);
-		}
-	}
-	return \%h;
-}
-
-sub decode_rfc3676 {		# RFC2231準拠
-	my $self = shift;
-	my $type = shift;
-	my $text = shift;
-
-	if ($type !~ m|text/plain|i || $type !~ /format=flowed/i) {
-		return $text;
-	}
-	$text =~ s/(^|\n) /$1/g;
-	if ($type =~ /delsp=yes/i) {
-		$text =~ s/ \r?\n//g;
-	} else {
-		$text =~ s/ \r?\n/ /g;
-	}
-	return $text;
-}
-
-sub decode_quoted_printable {	# Content-Transfer-Encoding: quoted-printable
-	my $self = shift;
-	my $ct_enc = shift;
-	my $text = shift;
-
-	if ($ct_enc !~ m|quoted-printable|i) {
-		return $text;
-	}
-	$text =~ s/=([0-9A-Fa-f][0-9A-Fa-f])/chr(hex($1))/eg;
-	$text =~ s/=\r?\n//sg;
-	return $text;
-}
-
 sub base64decode {	# 'normal' or 'URL safe'
 	my $self = shift;
 	my $str  = shift;
