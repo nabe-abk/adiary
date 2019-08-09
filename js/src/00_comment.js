96d7ee1ae4322c82e6e719e16c3034ba6c8dc5ee
nabe-abk <339449+nabe-abk@users.noreply.github.com>
2019-08-09 23:32:44 +0900

    Update: adiary.min.js

--- js/src/00_comment.js
+++ js/src/00_comment.js
@@ -1,9 +1,9 @@
-/*!
+/*!!
  * This file contains the following:
  *
  *	jQuery		(C)JS Foundation and other contributors
  * 	adiary.js	(C)nabe@abk
  *	lightbox.js	(C)Lokesh Dhakar
  *
- * For more information, see the comments and files under "src/".
+ * For more information, see the "src/" directory.
  */
