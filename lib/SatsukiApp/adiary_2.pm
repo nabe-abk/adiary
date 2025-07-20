3843d4c87c1b19c575dee5fccbb3ac907023f314
nabe-abk <339449+nabe-abk@users.noreply.github.com>
2025-07-20 16:54:52 +0900

    Update: changed whitespace handling in article descriptions.

--- lib/SatsukiApp/adiary_2.pm
+++ lib/SatsukiApp/adiary_2.pm
@@ -585,8 +585,10 @@ sub set_description {
 	}
 
 	$text = substr($text, 0, 4096);		# 長文への対策
-	$text =~ s/[\r\n]//g;
 	$ROBJ->tag_delete($text);
+	$text =~ s/\s+/ /g;
+	$text =~ s/^ //;
+	$text =~ s/ $//;
 	$h->{description} = $self->string_clip($text, $self->{blog}->{desc_len} || 64);
 }
 
