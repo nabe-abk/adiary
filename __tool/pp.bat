7b278f3f4a2db3a5ca4e2e86387159abcf319cdb
nabe-abk <339449+nabe-abk@users.noreply.github.com>
2025-07-22 16:32:45 +0900

    Update: remove release tools, and move to https://github.com/nabe-abk/adiary-release-tools

--- __tool/pp.bat
+++
@@ -1,6 +0,0 @@
-for /f "usebackq tokens=*" %%i in (`perl -e "use Image::Magick;my $p=$INC{'Image/Magick.pm'}; $p =~ s|/[^/]*$||; print $p"`) do @set MAGICK_PATH=%%i
-@rem
-pp @__tool/pp.opt -a %MAGICK_PATH%;site/lib/Image
-@rem
-@rem A checksum error occurs in the file with icon replaced
-@rem perl -e "use Win32::Exe; $exe = Win32::Exe->new('adiary.exe'); $exe->set_single_group_icon('__tool/pp.ico'); $exe->write;"
